f20aa0615e71743f389509188bda8346908a42d7
Shangzhi Pan <shangzhi.pan@go1.com>
2019-10-08 16:33:28 +1000

    Adding more custom rules to go1d shaker

--- .babelrc.js
+++ .babelrc.js
@@ -3,19 +3,15 @@ module.exports = {
     ["transform-imports", {
       "@go1d/go1d": {
         "transform":function (importName, matches) {
-          if (importName == "foundations") {
-            return `@go1d/go1d/build/foundations/index`;
+          switch(importName) {
+            case "foundations": return `@go1d/go1d/build/foundations/index`;
+            case "globalCSS": return `@go1d/go1d/build/foundations/globalCSS`;
+            case "DropdownItem": return `@go1d/go1d/build/components/Dropdown/DropdownItem`;
+            case "LI": return `@go1d/go1d/build/components/UL/LI`;
+            case "NotificationContainer": return `@go1d/go1d/build/components/Notification/NotificationContainer`;
+            // Checked till Letter "I"
+            default: return `@go1d/go1d/build/components/${importName}`;
           }
-          if (importName == "globalCSS") {
-            return `@go1d/go1d/build/foundations/globalCSS`;
-          }
-          if (importName == "globalCSS") {
-            return `@go1d/go1d/build/foundations/globalCSS`;
-          }
-          if (importName == "NotificationContainer") {
-            return `@go1d/go1d/build/components/Notification/NotificationContainer`;
-          }
-          return `@go1d/go1d/build/components/${importName}`;
         },
         "preventFullImport": true
       },

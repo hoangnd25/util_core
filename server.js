03f11f8a2b002dd74a24661358a37daf449228a7
Thuat Le Trong <thuat.le@go1.com>
2019-10-28 13:55:19 +0700

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const handle = routes.getRequestHandler(app);
 app.prepare().then(() => {
   const server = express();
 
-  server.use(handle);
+  server.get("*", (req, res) => {
+    /* serving page */
+    const { route, query } = routes.match(req.url);
+    if (!route) {
+      return handle(req, res);
+    }
+    return app.render(req, res, route.page, query);
+  });
 
   server.listen(port, err => {
     if (err) throw err;

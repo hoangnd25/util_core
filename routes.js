699cb9843a339449287fcfe264007645232aed85
Thuat Le Trong <thuat.le@go1.com>
2019-10-28 14:36:42 +0700

    routes.js

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const appUrlPrefix = process.env.NODE_ENV !== 'production' ? '' : '/r/app/portal
 
 module.exports = routes()
   // .add(NAME, PATTERN, PATH_TO_CHILD_FOLDER_INSIDE_PAGES)
-  .add('home', `${appUrlPrefix}/`, PAGES.USER_DATA_FEED)
+  .add('home', `${appUrlPrefix}`, PAGES.USER_DATA_FEED)
   .add('integrations', `${appUrlPrefix}/integrations`, PAGES.USER_DATA_FEED)
   .add('integrations.userDataFeed', `${appUrlPrefix}/integrations/user-data-feed`, PAGES.USER_DATA_FEED)
   ;

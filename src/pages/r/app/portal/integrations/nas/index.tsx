d19b3d327155986ae6ce611a0025b62be49b90f1
Michelle Bryant <michelle.bryant@go1.com>
2020-10-19 14:06:45 +1000

    updated design for ADP- mas/nas integration screen

--- src/pages/r/app/portal/integrations/nas/index.tsx
+++ src/pages/r/app/portal/integrations/nas/index.tsx
@@ -30,15 +30,6 @@ export class Nas extends React.Component<Props, any> {
     return (
       <Provider theme={theme as any}>
         <View flexDirection="column" minHeight="40vh">
-          <View justifyContent="center" borderBottom={1} borderColor="faded">
-            <Text fontSize={4} fontWeight="semibold" color="N900" textAlign="center">
-              Mas
-            </Text>
-            <Text textAlign="center" color="subtle" paddingBottom={4}>
-              Use the generated developer credentials to complete the integration.
-            </Text>
-          </View>
-
           <View flexDirection="column" width="100%" justifyContent="center" paddingTop={6}>
             <CopyTextInput fieldName="Portal name" fieldValue={currentSession.portal.title} />
             <CopyTextInput fieldName="Username" fieldValue="adp-admin@go1.com" />

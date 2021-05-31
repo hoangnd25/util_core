67bc2d5bdd859c1f44e8aa5d2a5c97556877d350
Michelle Bryant <michelle.bryant@go1.com>
2021-05-31 16:44:35 +1000

    linting

--- src/components/Settings/Theme/Previews/SplitLayout.tsx
+++ src/components/Settings/Theme/Previews/SplitLayout.tsx
@@ -28,103 +28,100 @@ const SplitLayout = props => {
       termsAgreement = 'By signing up';
 
       case 'log in':
-   
   }
   
   return (
-<Provider accent={foundations.colors.accent}>
-    <View width="100%" height="570px" flexDirection="row" border={1} borderColor="soft">
-      <View
-        width="400px"
-        height="100%"
-        borderColor="successLowest"
-        backgroundColor="successLowest"
-        overflow="hidden"
-        css={{
-          backgroundImage: landingPage,
-          backgroundPosition: 'center center',
-          backgroundSize: 'cover',
-        }}
-      ></View>
+    <Provider accent={foundations.colors.accent}>
+      <View width="100%" height="570px" flexDirection="row" border={1} borderColor="soft">
+        <View
+          width="400px"
+          height="100%"
+          borderColor="successLowest"
+          backgroundColor="successLowest"
+          overflow="hidden"
+          css={{
+            backgroundImage: landingPage,
+            backgroundPosition: 'center center',
+            backgroundSize: 'cover',
+          }}
+        ></View>
 
-      <View width="400px" backgroundColor="faint" height="100%" overflow="auto">
-        <View alignItems="center" justifyContent="center" width={'100%'} marginTop={8} paddingX={6}>
-          {/* LOGOs */}
-          {(logo && logo !== DEFAULT_APIOM_LOGO && (
-            <View
-              width="100%"
-              height={128}
-              marginY={6}
-              data-testid="portal-logo"
-              css={{
-                backgroundImage: `url(${logo})`,
-                backgroundPosition: 'center center',
-                backgroundSize: 'contain',
-                backgroundRepeat: 'no-repeat',
-              }}
-            ></View>
-          )) || <IconGo1Logo size={8} marginY={6} color="successLowest" />}
+        <View width="400px" backgroundColor="faint" height="100%" overflow="auto">
+          <View alignItems="center" justifyContent="center" width={'100%'} marginTop={8} paddingX={6}>
+            {/* LOGOs */}
+            {(logo && logo !== DEFAULT_APIOM_LOGO && (
+              <View
+                width="100%"
+                height={128}
+                marginY={6}
+                data-testid="portal-logo"
+                css={{
+                  backgroundImage: `url(${logo})`,
+                  backgroundPosition: 'center center',
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              ></View>
+            )) || <IconGo1Logo size={8} marginY={6} color="successLowest" />}
 
-          {/* TITLE */}
+            {/* TITLE */}
 
-          <Text
-            css={{ width: '100%' }} // explicitly set width for IE
-            marginBottom={3}
-            fontSize={3}
-            fontWeight="medium"
-            textAlign="center"
-            color="successLowest"
-          >
-            {primaryTagline}
-          </Text>
-          <Text marginBottom={5} color="default" textAlign="center">
-            {secondaryTagline}{' '}
-            <Text color="successLowest" fontWeight="medium" textAlign="center">
-              {link}
+            <Text
+              css={{ width: '100%' }} // explicitly set width for IE
+              marginBottom={3}
+              fontSize={3}
+              fontWeight="medium"
+              textAlign="center"
+              color="successLowest"
+            >
+              {primaryTagline}
             </Text>
-          </Text>
-          <View padding={4} borderRadius={3} marginBottom={5} backgroundColor="delicate">
-            <Text color="subtle">{signupDescription}</Text>
-          </View>
-
-          {previewType === 'sign up' && (
-            <View>
-              <View flexDirection="row" display="flex" marginY={3} justifyContent="space-between" width="100%">
-                <View paddingRight={1} flexShrink={1} flexGrow={1}>
-                  <TextInput id="firstName" label="First name" floating></TextInput>
-                </View>
-                <View paddingLeft={1} flexShrink={1} flexGrow={1}>
-                  <TextInput id="lastName" label="Last name" floating></TextInput>
-                </View>
-              </View>
-
-              <View width="100%">
-                <TextInput id="email" label="Email" floating></TextInput>
-              </View>
-              <View marginY={3} width="100%">
-                <PasswordInput id="password" label="Password" floating></PasswordInput>
-              </View>
+            <Text marginBottom={5} color="default" textAlign="center">
+              {secondaryTagline}{' '}
+              <Text color="successLowest" fontWeight="medium" textAlign="center">
+                {link}
+              </Text>
+            </Text>
+            <View padding={4} borderRadius={3} marginBottom={5} backgroundColor="delicate">
+              <Text color="subtle">{signupDescription}</Text>
             </View>
-            )}
 
-          <ButtonFilled marginY={4} width="100%" color="accent">
-            {buttonText}
-          </ButtonFilled>      
+            {previewType === 'sign up' && (
+              <View>
+                <View flexDirection="row" display="flex" marginY={3} justifyContent="space-between" width="100%">
+                  <View paddingRight={1} flexShrink={1} flexGrow={1}>
+                    <TextInput id="firstName" label="First name" floating></TextInput>
+                  </View>
+                  <View paddingLeft={1} flexShrink={1} flexGrow={1}>
+                    <TextInput id="lastName" label="Last name" floating></TextInput>
+                  </View>
+                </View>
 
-          <Text textAlign="center">
-              {termsAgreement}, you agree to the{' '}
-            <Text color="successLowest" fontWeight="bold" textDecoration="underline">
-              Go1 terms of use
-            </Text>{' '}
-              and{' '}
-            <Text color="successLowest" fontWeight="bold" textDecoration="underline">
-              privacy policy.
+                <View width="100%">
+                  <TextInput id="email" label="Email" floating></TextInput>
+                </View>
+                <View marginY={3} width="100%">
+                  <PasswordInput id="password" label="Password" floating></PasswordInput>
+                </View>
+              </View>
+              )}
+
+            <ButtonFilled marginY={4} width="100%" color="accent">
+              {buttonText}
+            </ButtonFilled>      
+            <Text textAlign="center">
+                {termsAgreement}, you agree to the{' '}
+              <Text color="successLowest" fontWeight="bold" textDecoration="underline">
+                Go1 terms of use
+              </Text>{' '}
+                and{' '}
+              <Text color="successLowest" fontWeight="bold" textDecoration="underline">
+                privacy policy.
+              </Text>
             </Text>
-          </Text>
-
+          </View>
         </View>
       </View>
-    </View>
     </Provider>
   );
 };

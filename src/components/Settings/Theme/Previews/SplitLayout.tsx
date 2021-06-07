1b771501f8e1b72c4269c7cc9d20c43590fe213e
Michelle Bryant <michelle.bryant@go1.com>
2021-06-07 20:02:27 +1000

    UI fixes

--- src/components/Settings/Theme/Previews/SplitLayout.tsx
+++ src/components/Settings/Theme/Previews/SplitLayout.tsx
@@ -4,100 +4,98 @@ import IconGo1Logo from '@go1d/go1d/build/components/Icons/Go1Logo';
 import { Trans } from '@lingui/macro';
 
 interface Props {
-  primaryTagline, description, logo, buttonText, featuredImage, terms: string;
+  primaryTagline;
+  description;
+  logo;
+  buttonText;
+  featuredImage;
+  terms: string;
   secondaryTagline: string[];
-  children: React.ReactNode, 
+  children: React.ReactNode;
 }
 
-const SplitLayout: React.FC<Props> = (props) => {
+const SplitLayout: React.FC<Props> = props => {
   const { primaryTagline, secondaryTagline, description, logo, buttonText, featuredImage, children, terms } = props;
 
   const DEFAULT_APIOM_LOGO = 'images/logo-white.png';
-  
+
   return (
-      <View width="100%" height="580px" flexDirection="row" border={1} borderColor="soft">
-        <View
-          width="400px"
-          height="100%"
-          borderColor="successLowest"
-          backgroundColor="successLowest"
-          overflow="hidden"
-          css={{
-            backgroundImage: featuredImage,
-            backgroundPosition: 'center center',
-            backgroundSize: 'cover',
-          }}
-        ></View>
+    <View width="100%" height="580px" flexDirection="row" border={1} borderColor="soft">
+      <View
+        width="400px"
+        height="100%"
+        borderColor="successLowest"
+        backgroundColor="successLowest"
+        overflow="hidden"
+        css={{
+          backgroundImage: featuredImage,
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover',
+        }}
+      ></View>
 
-        <View width="400px" backgroundColor="faint" height="100%" overflow="auto">
-          <View alignItems="center" justifyContent="center" width={'100%'} marginTop={8} paddingX={6}>
-            {/* LOGOs */}
-            {(logo && logo !== DEFAULT_APIOM_LOGO && (
-              <View
-                width="100%"
-                height={128}
-                marginY={5}
-                data-testid="portal-logo"
-                css={{
-                  backgroundImage: `url(${logo})`,
-                  backgroundPosition: 'center center',
-                  backgroundSize: 'contain',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              ></View>
-            )) || <IconGo1Logo size={8} marginY={6} color="successLowest" />}
+      <View width="400px" backgroundColor="faint" height="100%" overflow="auto">
+        <View alignItems="center" justifyContent="center" width={'100%'} marginTop={8} paddingBottom={4} paddingX={6}>
+          {/* LOGOs */}
+          {(logo && logo !== DEFAULT_APIOM_LOGO && (
+            <View
+              width="100%"
+              height={128}
+              marginY={5}
+              data-testid="portal-logo"
+              css={{
+                backgroundImage: `url(${logo})`,
+                backgroundPosition: 'center center',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+              }}
+            ></View>
+          )) || <IconGo1Logo size={8} marginY={6} color="successLowest" />}
 
+          <Text
+            css={{ width: '100%' }} // explicitly set width for IE
+            marginBottom={3}
+            fontSize={3}
+            fontWeight="medium"
+            textAlign="center"
+            color="successLowest"
+          >
+            {primaryTagline}
+          </Text>
 
-            <Text
-              css={{ width: '100%' }} // explicitly set width for IE
-              marginBottom={3}
-              fontSize={3}
-              fontWeight="medium"
-              textAlign="center"
-              color="successLowest"
-            >
-              {primaryTagline}
+          <Text marginBottom={5} color="default" textAlign="center">
+            {secondaryTagline[0]}{' '}
+            <Text color="successLowest" fontWeight="medium" textAlign="center">
+              {secondaryTagline[1]}
             </Text>
-  
-            <Text marginBottom={5} color="default" textAlign="center">
-              {secondaryTagline[0]}{' '}
-              <Text color="successLowest" fontWeight="medium" textAlign="center">
-              {secondaryTagline[1]} 
-              </Text>
-            </Text>
-       
+          </Text>
+
+          {description && (
             <View padding={4} borderRadius={3} marginBottom={5} backgroundColor="delicate">
               <Text color="subtle">{description}</Text>
             </View>
+          )}
+          {children}
 
-            <View zIndex={'modalBackdrop'}>
-              {children}
-          
-              <ButtonFilled marginY={4} width="100%" color="accent">
-                {buttonText}
-              </ButtonFilled>
+          <ButtonFilled marginY={4} width="100%" color="accent" css={{ pointerEvents: 'none' }}>
+            {buttonText}
+          </ButtonFilled>
 
-              {/* Overlay to prevent usage of form */}
-                <View backgroundColor="none" height={578} width={300} position="absolute" css={{ bottom: '10%' }} zIndex={'modal'} overflow="auto"/>
-            </View>
-                  
-            <Text textAlign="center">
-              {terms} 
+          <Text textAlign="center">
+            {terms}{' '}
+            <Trans>
               <Text color="successLowest" fontWeight="bold" textDecoration="underline">
-              {' '}
-                <Trans>Go1 terms of use</Trans>
-              </Text>
-              {' '}
-                <Trans>and</Trans>
-              {' '}
+                terms of use
+              </Text>{' '}
+              and{' '}
               <Text color="successLowest" fontWeight="bold" textDecoration="underline">
-                <Trans>privacy policy.</Trans>
+                privacy policy.
               </Text>
-            </Text>
-
-          </View> 
+            </Trans>
+          </Text>
         </View>
       </View>
+    </View>
   );
 };
 
--- src/components/Settings/Theme/SectionSignup.tsx
+++ src/components/Settings/Theme/SectionSignup.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Checkbox, Field, TextInput, View, PasswordInput  } from '@go1d/go1d';
+import { Checkbox, Field, TextInput, View  } from '@go1d/go1d';
 import { t, Trans } from '@lingui/macro';
 import { I18n } from '@lingui/react';
 import SettingsFormSection from '@src/components/Settings/SettingsFormSection';
 import { FunctionComponent } from 'react';
 import PreviewButton from '@src/components/Settings/Theme/PreviewButton';
 import Preview from '@src/components/Settings/Theme/Preview';
+import IconEye from '@go1d/go1d/build/components/Icons/Eye';
 import { FormValues } from '@src/components/Settings/Theme/Form';
 
 import getConfig from 'next/config';
@@ -42,29 +43,28 @@ const SectionSignup: FunctionComponent<Props> = ({ isPartnerPortal, themeSetting
             onRequestClose={() => setOpenPreview(false)} 
             title={i18n._(t`sign up`)}
             buttonText={i18n._(t`Create new account`)}
-            primaryTagline={signupTitle}
-            terms={i18n._(t`By signing up, you agree`)}
-            secondaryTagline={[i18n._(t`"Already have an account?`), i18n._(t`Log in`)]}
+            primaryTagline={signupTitle || 'Sign up with your work email '}
+            terms={i18n._(t`By creating an account you are agreeing to the Go1`)}
+            secondaryTagline={[i18n._(t`Already have an account?`), i18n._(t`Log in`)]}
             description={signupDescription}
             featuredImage={landingPage}
             logo={logo}>       
             <View>
               <View flexDirection="row" display="flex" marginY={3} justifyContent="space-between" width="100%">
                 <View paddingRight={1} flexShrink={1} flexGrow={1}>
-                  <TextInput id="firstName" label={i18n._(t`First Name`)} floating></TextInput>
+                  <TextInput id="firstName" label={i18n._(t`First Name`)} placeHolder="First Name" css={{ pointerEvents: 'none' }}></TextInput>
                 </View>
                 <View paddingLeft={1} flexShrink={1} flexGrow={1}>
-                  <TextInput id="lastName" label={i18n._(t`Last Name`)} floating></TextInput>
+                  <TextInput id="lastName" label={i18n._(t`Last Name`)} autoComplete="new-password" placeHolder="Last Name" css={{ pointerEvents: 'none' }}></TextInput>
                 </View>
               </View>
-
               <View width="100%">
-                <TextInput id="email" label={i18n._(t`Email`)} floating></TextInput>
+                <TextInput id="Email" placeHolder="Email" autoComplete="new-password" css={{ pointerEvents: 'none' }}></TextInput>
               </View>
               <View marginY={3} width="100%">
-                <PasswordInput id="password" label={i18n._(t`Password`)} floating></PasswordInput>
+                <TextInput id="Password" placeHolder="Password" autoComplete="new-password" css={{ pointerEvents: 'none' }} suffixNode={<View padding={4}><IconEye color="accent"/></View>}></TextInput>
               </View>
-          </View>
+            </View>
           </Preview>
 
           <View marginBottom={5}>
